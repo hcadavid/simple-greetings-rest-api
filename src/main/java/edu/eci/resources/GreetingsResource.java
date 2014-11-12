c62b26643d7af2d87afefc506900fd43943fabed
Hector Fabio Cadavid Rengifo <hcadavid@Hectors-MacBook-Pro.local>
2014-11-12 15:02:43 -0500

    release 0

--- src/main/java/edu/eci/resources/GreetingsResource.java
+++ src/main/java/edu/eci/resources/GreetingsResource.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package edu.eci.resources;
 
@@ -11,18 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author hcadavid
- */
 @RestController
-@RequestMapping("/protected/saludo")
-public class SampleResource {
+@RequestMapping("/saludo")
+public class GreetingsResource {
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public String getGreeting(@PathVariable String name) {
   
-        String result = "Hello " + name;
+        String result = "Hola " + name;
 
         return result;
 
